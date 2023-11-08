2d0d6d5a72e181f770dc8918ba3fd5aeaca6c984
sers1 <sers156@mail.ru>
2023-11-08 16:32:18 +1000

    hw7

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,29 @@
 public class BankAccount {
+
+    double balanc = 1000;
+
+    void popolnenieScheta(double summaPopolnenie){
+        System.out.println("Баланс до пополнения:" + balanc);
+        System.out.println("Баланс пополнится на:" + summaPopolnenie);
+        balanc+=summaPopolnenie;
+        System.out.println("Баланс после пополнения:" + balanc);
+    }
+
+    void snyatieSoscheta(double summaSnyatie){
+        System.out.println("Баланс до снятия:" + balanc);
+        System.out.println("С баланса снимется:" + summaSnyatie);
+        balanc-=summaSnyatie;
+        System.out.println("Баланс после снятия:" + balanc);
+
+    }
 }
+
+class BankAccountTest{
+    public static void main(String[] args) {
+        BankAccount MyBalac= new BankAccount();
+        MyBalac.popolnenieScheta(80);
+        MyBalac.snyatieSoscheta(50);
+
+    }
+}
+
--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,38 @@
 public class Employee {
+
+    int id;
+    String surname;
+    int age;
+    int salary;
+    String department;
+
+    Employee( int id2, String surname2, int age2, int salary2, String department2) {
+        id = id2;
+        surname = surname2;
+        age = age2;
+        salary = salary2;
+        department = department2;
+    }
+
+    double uvelichenieSalary(){
+        salary *= 2;
+        return salary;
+
+    }
 }
+
+class EmployeeTest {
+
+    public static void main(String[] args) {
+
+        Employee em1 = new Employee(1, "Попов", 30, 30000, "Строитель");
+        Employee em2 = new Employee(2, "Парыгин", 20, 60000, "Врач");
+
+        em1.uvelichenieSalary();
+        System.out.println("Новая зарплата работника" +  " " + em1.surname  + "="  + em1.salary);
+
+        em2.uvelichenieSalary();
+        System.out.println("Новая зарплата работника" + " " + em2.surname  + "=" + em2.salary);
+    }
+}
+
